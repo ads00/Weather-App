e57035c33351b9824d08c22db55dc3715d4d32c6
ads00 <adshayanbalachandran@gmail.com>
2021-07-07 12:42:18 +0530

    Getting data from API done

--- weather.js
+++ weather.js
@@ -1,1 +1,22 @@
-`api.openweathermap.org/data/2.5/weather?q=${city_name},${state_code}&appid=b1148d5922d66cf603c545b800f5e379`
+class Weather {
+    constructor(city, country) {
+        this.apiKey = 'b1148d5922d66cf603c545b800f5e379';
+        this.city = city;
+        this.country = country;
+    }
+
+    // Get weather from API
+    async getWeather() {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.country}&appid=${this.apiKey}`);
+
+        const responseData = await response.json();
+
+        return responseData;
+    }
+
+    // Change weather location
+    changeLocation(city, country) {
+        this.city = city;
+        this.country = country;
+    }
+}
